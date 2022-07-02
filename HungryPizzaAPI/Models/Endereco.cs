28581ce8b8a3e9b6fdd0885b16fb472f3f9b59f1
togo88-global <tiagogb_2@hotmail.com>
2022-07-01 21:02:34 -0300

    Add project files.

---
+++ HungryPizzaAPI/Models/Endereco.cs
@@ -0,0 +1,11 @@
+﻿namespace HungryPizzaAPI.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Rua { get; set; }
+        public string CEP { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
---
+++ HungryPizzaAPI/Models/Sabores.cs
@@ -0,0 +1,10 @@
+﻿namespace HungryPizzaAPI.Models
+{
+    public class Sabores
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public bool EmFalta { get; set; } = false;
+    }
+}
